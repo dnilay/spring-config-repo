12ac35f85859afd60c25f6b194072938cc2d95c5
nilay <nilay@nilay-HP-EliteBook-840-G2>
2020-06-12 14:32:33 +0530

    commit

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,13 +18,14 @@ import org.springframework.web.client.RestTemplate;
 public class CurrencyConversionController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	@Autowired
+	private Environment environment;
 
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-
-		// Feign - Problem 1
+System.out.println(environment.getProperty("local.server.port"));
+	
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
